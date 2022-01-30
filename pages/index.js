6434d47775acc21ffb8aa91353f2c6bc4109dbf8
Nate <natoraider@gmail.com>
2022-01-29 23:13:49 -0700

    fetch content

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,26 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
-export default () => (
-  <div>Hello World. <Link href='/about' as={ process.env.BACKEND_URL + '/about'}><a>About</a></Link></div>
-)
+
+export default () => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No data</p>
+
+  return (
+    <div>
+      <h1># Posts: {data.count}</h1>
+    </div>
+  )
+}
